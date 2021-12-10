ef5ca83974c48b00af7c8197f951fd9369f25614
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-12-10 09:23:45 -0800

    Complete App.js - connect component to the fetchSmurfs action.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React, { Component } from "react";
-
+//Task List:
+import React, { useEffect } from "react";
+//1. Connect the fetchSmurfs actions to the App component.
+import { connect } from "react-redux";
 import AddForm from './components/AddForm';
+import { fetchSmurfs } from "./actions";
 import SmurfList from './components/SmurfList';
 import Header from './components/Header';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 
-const App = ()=> {
+const App = ( props ) => {
+
+  //2. Call the fetchSmurfs action when the component mounts.
+  useEffect (() => {
+    props.fetchSmurfs()   
+  }, [])
+
   return (
     <div className="App">
       <Header />
@@ -19,9 +28,9 @@ const App = ()=> {
     </div>
   );
 }
+const mapStateToProps = ( state ) => {
+  return state;
+}
+export default connect(mapStateToProps, { fetchSmurfs })(App);
 
-export default App;
 
-//Task List:
-//1. Connect the fetchSmurfs actions to the App component.
-//2. Call the fetchSmurfs action when the component mounts.
