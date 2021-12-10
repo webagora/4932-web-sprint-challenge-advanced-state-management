624c585c70a5400fabffe049aaf63a4b2cf1aa61
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-12-10 10:06:14 -0800

    Complete components/SmurfList.js - map SmurfList to Smurf.

--- src/components/SmurfList.js
+++ src/components/SmurfList.js
@@ -3,29 +3,25 @@ import { connect } from 'react-redux';
 import Smurf from './Smurf';
 
  const SmurfList = (props)=> {
-    console.log('props in SmurfList: ', props);
-    const { smurfs, isLoading } = props;
-
-    const testSmurf = {
-        id:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
-        name:'Poppa Smurf',
-        position:'Village Leader',
-        nickname: 'Pops',
-        description: 'Papa is the practical village leader and the father figure of 100 or so young Smurfs. He is easily identified by his red Smurf hat, pants, and a shortly-trimmed white beard and moustache.'
-    }
+    
+    const { smurfs } = props;
+   
+    const isLoading = false;
 
     if (isLoading) {
         return <h1>Loading...</h1>;
     }    
 
     return(<div className="listContainer">
-        <Smurf smurf={testSmurf}/>
+        {
+            smurfs.map(smurf => <Smurf key ={smurf.id} smurf={smurf}/>)
+        }        
     </div>);
 }
 
 //Task List:
 //1. Connect the smurfs and loading state values to the SmurfList component.
-const mapStateToProps = ({ smurfs, isLoading }) => ({ smurfs, isLoading});
+const mapStateToProps = ({ smurfs }) => ({ smurfs });
 
 export default connect(mapStateToProps)(SmurfList);
 
