821f6b9f172fbdfe560d17adfe79741a7accca82
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-12-10 08:59:27 -0800

    Complete reducers/index.js - complete a standard reducer function.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -12,14 +12,18 @@ const initialState = {
     error: ""
 }
 
-const reducer = (state , action) => {
+const reducer = (state = initialState, action) => {
     //2. Add in the arguments needed to complete a standard reducer function.
     //3. Add in a reducer case to accommodate the start of a smurf fetch.
     //4. Add in a reducer case to accommodate the successful smurf api fetch.
     //5. Add in a reducer cases to accommodate the failed smurf api fetch.
     //6. Add in a reducer case to accommodate adding a smurf (including the name, nickname, position, summary and an internally generated id) into your smurf list.
     //7. Add in a reducer case that adds in a value to the error message.
-    
+    switch (action.type){
+        
+        default:
+            return state  
+    }       
 }
 
 export default reducer
