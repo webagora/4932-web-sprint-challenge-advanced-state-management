cc5f7c6beeec6dc5577334152e2f8b6b0d6dc67a
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-12-10 09:18:03 -0800

    Complete actions/index.js - Add a thunk action called fetchSmurfs.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,24 @@
 import axios from 'axios';
-
 //Task List:
+
+export const LOADING = "LOADING";
+export const SUCCESS = "SUCCESS";
+export const ERROR = "ERROR";
+export const ADD_SMURF = "ADD_SMURF";
+
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
+export const fetchSmurfs = () => (dispatch) => {
+    dispatch({type: LOADING})
+    
+    axios.get(`http://localhost:3333/smurfs`)
+        .then (resp => {            
+            dispatch({type: SUCCESS, payload: resp.data})
+        })
+        .catch( err => {
+            //3. Add a standard action that allows us to set the value of the error message slice of state.            
+            dispatch({type: ERROR, payload: err.message })
+        })    
+}
+
 //2. Add a standard action that allows us to add new smurf (including the name, nickname, position, summary)
-//3. Add a standard action that allows us to set the value of the error message slice of state.
+
