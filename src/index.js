7d279266e7031d1eb19c308fe3f07dd2bf29d2db
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-12-10 09:13:50 -0800

    Complete index.js - Connect app to reducer through redux with the thunk and logger.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
+//Task List:
+//1. Add in all necessary components and libary methods.
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
+import reducer from "./reducers";
+
 import "./index.css";
 import App from "./App";
+
+//2. Create a store that includes thunk and logger middleware support.
+
+const store = createStore (
+    reducer,
+    applyMiddleware(thunk, logger)
+)
 
 const { worker } = require('./mocks/browser');
 worker.start();
@@ -10,11 +27,12 @@ worker.start();
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
-    <App />, 
+    //3. Wrap the App component in a react-redux Provider element.
+    <Provider store = { store } >
+        <App />
+    </Provider>, 
     rootElement
 );
 
-//Task List:
-//1. Add in all necessary components and libary methods.
-//2. Create a store that includes thunk and logger middleware support.
-//3. Wrap the App component in a react-redux Provider element.
+
+
