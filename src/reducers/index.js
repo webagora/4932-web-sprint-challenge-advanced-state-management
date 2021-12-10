a207abf1dbae23d0caadf73c6d0281910e102250
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-12-10 08:56:00 -0800

    Complete reducers/index.js - adds the initialState.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,22 +1,27 @@
 
-export const initialState = {
-}
-
-const reducer = ()=>{
-}
-
-//**************DO NOT EDIT ANY CODE BEYOND THIS POINT**************//
-export default reducer;
+import AddForm from "../components/AddForm"
 
 //Task List:
-//1. Adds the following state values into the initialState:
-//  - an array of smurfs
-//  - a boolean indicating if the app is loading
-//  - a string indicating a possible error message
+const initialState = {
+    //1. Adds the following state values into the initialState:
+    //  - an array of smurfs
+    //  - a boolean indicating if the app is loading
+    //  - a string indicating a possible error message
+    smurfs: [],
+    isLoading: false,
+    error: ""
+}
 
-//2. Add in the arguments needed to complete a standard reducer function.
-//3. Add in a reducer case to accomidate the start of a smurf fetch.
-//4. Add in a reducer case to accomidate the successful smurf api fetch.
-//5. Add in a reducer cases to accomidate the failed smurf api fetch.
-//6. Add in a reducer case to accomidate adding a smurf (including the name, nickname, position, summary and an internally generated id) into your smurf list.
-//7. Add in a reducer case that adds in a value to the error message.
+const reducer = (state , action) => {
+    //2. Add in the arguments needed to complete a standard reducer function.
+    //3. Add in a reducer case to accommodate the start of a smurf fetch.
+    //4. Add in a reducer case to accommodate the successful smurf api fetch.
+    //5. Add in a reducer cases to accommodate the failed smurf api fetch.
+    //6. Add in a reducer case to accommodate adding a smurf (including the name, nickname, position, summary and an internally generated id) into your smurf list.
+    //7. Add in a reducer case that adds in a value to the error message.
+    
+}
+
+export default reducer
+
+
